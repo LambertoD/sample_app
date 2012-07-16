8d8687f0ba969d7c7707aa1741659e34a2b3b68c
Lamberto Diwa <lambertodiwajr@att.net>
2012-07-15 20:18:20 -0700

    Finish user edit, update, index, and destroy actions

--- features/step_definitions/authentication_steps.rb
+++ features/step_definitions/authentication_steps.rb
@@ -7,7 +7,7 @@ When /^he submits invalid signin information$/ do
 end
 
 Then /^he should see an error message$/ do
-  page.should have_error_message('Invalid')
+  page.should have_selector('div.alert.alert-error', text: 'Invalid')
 end
 
 Given /^the user has an account$/ do
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,9 +1,24 @@
-FactoryGirl.define do 
+# FactoryGirl.define do 
+#   factory :user do
+#     name       "Michael Hartl"
+#     email      "michael@example.com"
+#     password   "foobar"
+#     password_confirmation "foobar"
+#   end
+# end
+# Note: updated above to allow creation of one or multiple users
+
+FactoryGirl.define do
   factory :user do
-    name       "Michael Hartl"
-    email      "michael@example.com"
-    password   "foobar"
+    sequence(:name)   { |n| "Person #{n}" }
+    sequence(:email)  { |n| "person_#{n}@example.com" }
+    password "foobar"
     password_confirmation "foobar"
+
+    factory :admin do
+      admin true
+    end
   end
-	
-end
+end 
+
+
